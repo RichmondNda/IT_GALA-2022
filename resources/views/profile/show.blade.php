f292fc0d01104111cbd5d0f80fad24762ef06eab
RichmondNda <ndaregisrichmond@gmail.com>
2022-06-05 14:39:44 +0000

    suppression de modification du pseudonyme

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,11 +7,12 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+           
+            {{-- @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
 
                 <x-jet-section-border />
-            @endif
+            @endif --}}
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
                 <div class="mt-10 sm:mt-0">
