fa1cc093f88992f1a5ccc0cfc449269477e2742b
RichmondNda <ndaregisrichmond@gmail.com>
2022-06-06 09:48:05 +0000

    correction redirection page d'acceuil

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -23,7 +23,7 @@
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laravel.com/docs">Première Partie</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">Première Partie</a></div>
         </div>
 
         <div class="ml-12">
@@ -40,7 +40,7 @@
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laracasts.com">Seconde partie</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">Seconde partie</a></div>
         </div>
 
         <div class="ml-12">
